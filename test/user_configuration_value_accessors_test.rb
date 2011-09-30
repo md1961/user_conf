7dbf3f4dc081794d8a0f04bd7e05ab0a246d67a6
Naoyuki Kumagai <kumagai@mimas>
2011-09-30 17:04:19 +0900

    UserConfigurationValueAccessorsTest to test get with unknown name
    test/user_configuration_value_accessors_test.rb
    - Move the first set_conf_value() call to setup() from test_set_and_get().
    - Add test_get_with_unknown_name().

--- test/user_configuration_value_accessors_test.rb
+++ test/user_configuration_value_accessors_test.rb
@@ -7,11 +7,17 @@ class UserConfigurationValueAccessorsTest < ActiveSupport::TestCase
 
   def setup
     @user = User.create!(name: 'whoever', password: 'whatever')
+    @user.set_conf_value(NAME, VALUE)
   end
 
   def test_set_and_get
-    @user.set_conf_value(NAME, VALUE)
     assert_equal(VALUE, @user.get_conf_value(NAME), "User#get_conf_value() after set_conf_value()")
+  end
+
+  def test_get_with_unknown_name
+    assert_raise(NameError) do
+      @user.get_conf_value(:you_never_know)
+    end
   end
 end
 
