d1dee289746dd6d82b0015fe97dc59d1d5ab906e
Naoyuki Kumagai <kumagai@mimas>
2011-09-30 17:08:24 +0900

    UserConfigurationValueAccessorsTest to test set & get with Array
    test/user_configuration_value_accessors_test.rb
    - Add test_set_and_get_with_an_array().

--- test/user_configuration_value_accessors_test.rb
+++ test/user_configuration_value_accessors_test.rb
@@ -14,9 +14,17 @@ class UserConfigurationValueAccessorsTest < ActiveSupport::TestCase
     assert_equal(VALUE, @user.get_conf_value(NAME), "User#get_conf_value() after set_conf_value()")
   end
 
+  ARRAY = [1, 'two', :three]
+
+  def test_set_and_get_with_an_array
+    name = :an_array
+    @user.set_conf_value(name, ARRAY)
+    assert_equal(ARRAY, @user.get_conf_value(name), "User#get_conf_value() after set_conf_value() with an Array")
+  end
+
   def test_get_with_unknown_name
     assert_raise(NameError) do
-      @user.get_conf_value(:you_never_know)
+      @user.get_conf_value(:you_never_know_this)
     end
   end
 end
