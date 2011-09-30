2e2a94269b6ba24d5bcfefac11ba7caae4e003b7
Naoyuki Kumagai <kumagai@mimas>
2011-09-30 16:19:19 +0900

    UserConfigurationValueTest to test the validation
    test/user_configuration_value_test.rb
    - Modify setup() to prepare @user and @obj_name.
    - Modify test_belongs_to_user() to use @user.
    - Add test_validates_user_configuration_name_id_for_uniqueness().

--- test/user_configuration_value_test.rb
+++ test/user_configuration_value_test.rb
@@ -2,17 +2,25 @@ require 'test_helper'
 
 class UserConfigurationValueTest < ActiveSupport::TestCase
 
-  VALUE = 'hoge'
-  NAME_ID = 0
+  NAME  = 'hoge'
+  VALUE = 'valuable'
 
   def setup
-    @obj_value = UserConfigurationValue.new(user_configuration_name_id: NAME_ID, value: VALUE)
+    @user      = User.create!(name: :whoever, password: 'whatever')
+    @obj_name  = UserConfigurationName.create!(name: NAME)
+    @obj_value = UserConfigurationValue.new(user_configuration_name: @obj_name, value: VALUE)
   end
 
   def test_belongs_to_user
-    user = User.create!(name: :whoever, password: 'whatever')
-    user.user_configuration_values << @obj_value
-    assert_equal(user, @obj_value.user, "User whom the UserConfigurationValue belongs to")
+    @user.user_configuration_values << @obj_value
+    assert_equal(@user, @obj_value.user, "User whom the UserConfigurationValue belongs to")
+  end
+
+  def test_validates_user_configuration_name_id_for_uniqueness
+    @user.user_configuration_values << @obj_value
+    assert_raise(ActiveRecord::RecordInvalid) do
+      @user.user_configuration_values.create!(user_configuration_name: @obj_name, value: VALUE)
+    end
   end
 end
 
