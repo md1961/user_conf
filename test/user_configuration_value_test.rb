441ec7e42d689115633da48c759359a9736f24a9
Naoyuki Kumagai <kumagai@mimas>
2011-09-30 16:27:43 +0900

    UserConfigurationValueTest to test the validation to pass
    test/user_configuration_value_test.rb
    - Add test_validates_user_configuration_name_id_for_uniqueness_to_pass().

--- test/user_configuration_value_test.rb
+++ test/user_configuration_value_test.rb
@@ -6,7 +6,7 @@ class UserConfigurationValueTest < ActiveSupport::TestCase
   VALUE = 'valuable'
 
   def setup
-    @user      = User.create!(name: :whoever, password: 'whatever')
+    @user      = User.create!(name: 'whoever', password: 'whatever')
     @obj_name  = UserConfigurationName.create!(name: NAME)
     @obj_value = UserConfigurationValue.new(user_configuration_name: @obj_name, value: VALUE)
   end
@@ -20,6 +20,20 @@ class UserConfigurationValueTest < ActiveSupport::TestCase
     @user.user_configuration_values << @obj_value
     assert_raise(ActiveRecord::RecordInvalid) do
       @user.user_configuration_values.create!(user_configuration_name: @obj_name, value: VALUE)
+    end
+  end
+
+  def test_validates_user_configuration_name_id_for_uniqueness_to_pass
+    @user.user_configuration_values << @obj_value
+
+    obj_name2 = UserConfigurationName.create!(name: NAME + '2')
+    assert_nothing_raised("Same user and different name should pass validation") do
+      @user.user_configuration_values.create!(user_configuration_name: obj_name2, value: VALUE)
+    end
+
+    user2 = User.create!(name: 'whoever2', password: 'whatever')
+    assert_nothing_raised("Same name and different user should pass validation") do
+      user2.user_configuration_values.create!(user_configuration_name: @obj_name, value: VALUE)
     end
   end
 end
