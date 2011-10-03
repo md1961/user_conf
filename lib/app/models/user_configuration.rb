6d15f6be0026d01a92c9d0595907bcb2eb5edfb1
Naoyuki Kumagai <kumagai@mimas>
2011-10-03 14:05:39 +0900

    Modify UserConfiguration.name=() to define getter and setter
    lib/app/models/user_configuration.rb
    - Modify self.name=() to define a getter and a setter for
      each attribute using private create_getter_and_setter().
    - Add user(), user=() and user_id=().
    test/user_configuration_test.rb
    - Add tests for the above.

--- lib/app/models/user_configuration.rb
+++ lib/app/models/user_configuration.rb
@@ -8,7 +8,34 @@ class UserConfiguration
     names.map(&:to_s).each do |name|
       next if UserConfigurationName.find_by_name(name)
       UserConfigurationName.create!(name: name)
+      create_getter_and_setter(name)
     end
   end
+
+  def user
+    return @user
+  end
+
+  def user=(user)
+    raise "Argument user must be a User" unless user.is_a?(User)
+    @user = user
+  end
+
+  def user_id=(id)
+    @user = User.find(id)
+  end
+
+  private
+
+    def self.create_getter_and_setter(name)
+      define_method(name) do
+        @user.get_conf_value(name)
+      end
+
+      define_method(name + '=') do |value|
+        @user.set_conf_value(name, value)
+      end
+    end
+    private_class_method :create_getter_and_setter
 end
 
--- test/user_configuration_test.rb
+++ test/user_configuration_test.rb
@@ -16,5 +16,36 @@ class UserConfigurationTest < ActiveSupport::TestCase
       assert_not_nil(UserConfigurationName.find_by_name(name.to_s), "UserConfigurationName with a name of '#{name}' should exist")
     end
   end
+
+  def test_self_names_eq_for_creating_getter_and_setter
+    UserConfiguration.names = [:an_attribute]
+
+    uc = UserConfiguration.new
+    uc.user = User.create!(name: 'whoever', password: 'whatever')
+
+    the_value = :the_value_you_never_guess
+    uc.an_attribute = the_value
+    assert_equal(the_value, uc.an_attribute, "UserConfiguration#an_attribute()")
+  end
+
+  def test_user
+    uc = UserConfiguration.new
+    uc.instance_variable_set(:@user, :a_user_mock)
+    assert_equal(:a_user_mock, uc.user, "UserConfiguration.user()")
+  end
+
+  def test_user_eq
+    user = User.new
+    uc = UserConfiguration.new
+    uc.user = user
+    assert_equal(user, uc.instance_variable_get(:@user), "@user of UserConfiguration")
+  end
+
+  def test_user_id_eq
+    user = User.create!(name: 'whoever', password: 'whatever')
+    uc = UserConfiguration.new
+    uc.user_id = user.id
+    assert_equal(user, uc.instance_variable_get(:@user), "@user of UserConfiguration")
+  end
 end
 
