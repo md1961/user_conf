ee763f28541e0bc2abd063b2822a40a469b317a4
Naoyuki Kumagai <kumagai@mimas>
2011-10-03 11:50:29 +0900

    UserConfigurationValueAccessors#set_conf_value() to reject unknown names
    lib/app/models/user_configuration_value_accessors.rb
    - Extract get_user_configuration_name() from get_conf_value().
    - Alter set_conf_value() to use newly extracted
      get_user_configuration_name().  As a result, the mothod raises
      NameError if it cannot find a UserConfigurationName with the name.
    test/user_configuration_value_accessors_test.rb
    - Modify setup() to set UserConfiguration.names.

--- lib/app/models/user_configuration_value_accessors.rb
+++ lib/app/models/user_configuration_value_accessors.rb
@@ -6,37 +6,29 @@ module UserConfigurationValueAccessors
   end
 
   def get_conf_value(name)
-    obj_name = UserConfigurationName.find_by_name(name.to_s)
-    raise NameError, "No value entry with a name of '#{name}'" unless obj_name
-
+    obj_name = get_user_configuration_name(name)
     obj_value = user_configuration_values.detect { |value| value.user_configuration_name == obj_name }
+
     return obj_value && Marshal.load(obj_value.value)
   end
 
   def set_conf_value(name, value)
-    obj_name = UserConfigurationName.find_by_name(name.to_s)
-    if obj_name
-      clazz_name = obj_name.clazz
-    else
-      obj_name = UserConfigurationName.new(name: name.to_s)
-      clazz_name = nil
-    end
-
-    if clazz_name
-      unless value.class.name == clazz_name
-        raise TypeError, "The class of the given value (#{value.class}) does not match the class in DB (#{clazz_name})"
-      end
-    else
-      obj_name.clazz = value.class.name
-    end
-
-    obj_name.save!
-
+    obj_name = get_user_configuration_name(name)
+    clazz_name = obj_name.clazz
     obj_value = user_configuration_values.detect { |value| value.user_configuration_name == obj_name }
+
+    if obj_value.nil?
+      obj_name.clazz = value.class.name
+    elsif value.class.name != clazz_name
+      raise TypeError, "The class of the given value (#{value.class}) does not match the class in DB (#{clazz_name})"
+    end
+
     unless obj_value
       obj_value = UserConfigurationValue.new(user_id: id, user_configuration_name_id: obj_name.id)
     end
     obj_value.value = Marshal.dump(value)
+
+    obj_name .save!
     obj_value.save!
 
     # DB から読み直して、たった今設定した UserConfigurationValue を反映させる
@@ -44,5 +36,14 @@ module UserConfigurationValueAccessors
 
     nil
   end
+
+  private
+
+    def get_user_configuration_name(name)
+      obj_name = UserConfigurationName.find_by_name(name.to_s)
+      raise NameError, "No value entry with a name of '#{name}'" unless obj_name
+
+      return obj_name
+    end
 end
 
--- test/user_configuration_value_accessors_test.rb
+++ test/user_configuration_value_accessors_test.rb
@@ -2,31 +2,35 @@ require 'test_helper'
 
 class UserConfigurationValueAccessorsTest < ActiveSupport::TestCase
 
-  NAME  = :hoge
+  NAME_HOGE  = :hoge
+  NAME_ARRAY = :an_array
+  NAME_USER  = :user_defined
+  NAMES = [NAME_HOGE, NAME_ARRAY, NAME_USER]
+
   VALUE = 'valuable'
 
   def setup
+    UserConfiguration.names = NAMES
+
     @user = User.create!(name: 'whoever', password: 'whatever')
-    @user.set_conf_value(NAME, VALUE)
+    @user.set_conf_value(NAME_HOGE, VALUE)
   end
 
   def test_set_and_get
-    assert_equal(VALUE, @user.get_conf_value(NAME), "User#get_conf_value() after set_conf_value()")
+    assert_equal(VALUE, @user.get_conf_value(NAME_HOGE), "User#get_conf_value() after set_conf_value()")
   end
 
   ARRAY = [1, 'two', :three]
 
   def test_set_and_get_with_an_array
-    name = :an_array
-    @user.set_conf_value(name, ARRAY)
-    assert_equal(ARRAY, @user.get_conf_value(name), "User#get_conf_value() with an Array")
+    @user.set_conf_value(NAME_ARRAY, ARRAY)
+    assert_equal(ARRAY, @user.get_conf_value(NAME_ARRAY), "User#get_conf_value() with an Array")
   end
 
   def test_set_and_get_with_a_user_defined_class
-    name  = :user_defined
     value = UserDefinedClass.new(VALUE)
-    @user.set_conf_value(name, value)
-    actual = @user.get_conf_value(name)
+    @user.set_conf_value(NAME_USER, value)
+    actual = @user.get_conf_value(NAME_USER)
     assert_not_nil(actual, "User#get_conf_value() with an UserDefinedClass")
     assert_equal(UserDefinedClass, actual.class, "Class of User#get_conf_value() with an UserDefinedClass")
     assert_equal(VALUE, actual.value, "value of User#get_conf_value() with an UserDefinedClass")
@@ -40,7 +44,7 @@ class UserConfigurationValueAccessorsTest < ActiveSupport::TestCase
 
   def test_set_with_different_type
     assert_raise(TypeError) do
-      @user.set_conf_value(NAME, Object.new)
+      @user.set_conf_value(NAME_HOGE, Object.new)
     end
   end
 
