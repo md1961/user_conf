da1fd326a7f9820f92d5c4f0f637e843ccfdc626
Naoyuki Kumagai <kumagai@mimas>
2011-10-03 15:07:59 +0900

    Modify all_tests.rb to use constant CURDIR
    all_tests.rb
    - Introduce CURDIR.

--- all_tests.rb
+++ all_tests.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
-require File.join(File.dirname(__FILE__), 'test', 'user_configuration_name_test')
-require File.join(File.dirname(__FILE__), 'test', 'user_configuration_value_test')
-require File.join(File.dirname(__FILE__), 'test', 'user_configuration_value_accessors_test')
+CURDIR = File.dirname(__FILE__)
+
+require File.join(CURDIR, 'test', 'user_configuration_name_test')
+require File.join(CURDIR, 'test', 'user_configuration_value_test')
+require File.join(CURDIR, 'test', 'user_configuration_value_accessors_test')
 
