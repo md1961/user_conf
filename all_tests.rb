a507996d16db91ba6d34cbb151bf045c561179b3
Naoyuki Kumagai <kumagai@mimas>
2011-10-04 13:59:53 +0900

    Modify all_tests.rb to display filenames of the test codes
    all_tests.rb
    - Modify to display the filenames of the test codes to use.

--- all_tests.rb
+++ all_tests.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 TEST_DIR = File.join(File.dirname(__FILE__), 'test')
+TESTERS = Dir.entries(TEST_DIR).grep(/_test\.rb$/)
 
-Dir.entries(TEST_DIR).grep(/_test\.rb$/).each do |filename|
+INDENT = ' ' * 2
+puts "Testing with the followings in directory '#{TEST_DIR}':\n" \
+     + TESTERS.map { |x| INDENT + x }.join("\n") + "\n\n"
+
+TESTERS.each do |filename|
   require File.join(TEST_DIR, filename)
 end
 
